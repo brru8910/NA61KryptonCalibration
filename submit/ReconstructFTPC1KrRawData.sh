31e006fb1735d32ea6b5ff1460a1201641057e01
Brant Rumberger <brant.rumberger@cern.ch>
2022-11-13 13:05:46 +0100

    Updating to latest calibration package.

---
+++ submit/ReconstructFTPC1KrRawData.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+./condor_submit-CalibratedGains.sh run-041192x00001_vbuf2_2022-11-11_18-04-07.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00001_vbuf3_2022-11-11_18-04-07.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00002_vbuf1_2022-11-11_18-17-45.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00002_vbuf2_2022-11-11_18-17-45.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00002_vbuf3_2022-11-11_18-17-45.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00003_vbuf1_2022-11-11_18-32-12.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00003_vbuf2_2022-11-11_18-32-13.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00003_vbuf3_2022-11-11_18-32-12.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00003_vbuf4_2022-11-11_18-32-13.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00004_vbuf1_2022-11-11_18-46-50.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00004_vbuf2_2022-11-11_18-46-50.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00004_vbuf3_2022-11-11_18-46-50.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00004_vbuf4_2022-11-11_18-46-51.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00005_vbuf1_2022-11-11_19-04-06.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00005_vbuf2_2022-11-11_19-04-06.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00005_vbuf3_2022-11-11_19-04-06.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00005_vbuf4_2022-11-11_19-04-06.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00006_vbuf1_2022-11-11_19-25-55.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00006_vbuf2_2022-11-11_19-25-56.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00006_vbuf3_2022-11-11_19-25-56.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00006_vbuf4_2022-11-11_19-25-56.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00007_vbuf1_2022-11-11_19-47-47.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00007_vbuf2_2022-11-11_19-47-47.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00007_vbuf3_2022-11-11_19-47-48.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
+./condor_submit-CalibratedGains.sh run-041192x00007_vbuf4_2022-11-11_19-47-48.pteraw /eos/experiment/na61/data/OnlineProduction/2022-KrCalibration-OfflineQA/ChunkStorage
---
+++ submit/ReconstructGTPCKrRawData.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./condor_submit-CalibratedGains.sh run-041219x00001_vbuf1_2022-11-12_19-59-52.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00001_vbuf2_2022-11-12_19-59-52.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00001_vbuf3_2022-11-12_19-59-52.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00001_vbuf4_2022-11-12_19-59-52.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00002_vbuf1_2022-11-12_20-18-07.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00002_vbuf2_2022-11-12_20-18-07.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00002_vbuf3_2022-11-12_20-18-07.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
+./condor_submit-CalibratedGains.sh run-041219x00002_vbuf4_2022-11-12_20-18-07.pteraw /afs/cern.ch/user/n/na61qa/EOSDropDirectory/ChunkStorage/GTPC
